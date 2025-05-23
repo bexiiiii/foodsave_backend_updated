49c8be1161080b1a1f7089ee2936fb4be2a4e98e
Dynamite01 <behruzbektohtamishov@gmail.com>
2025-05-24 02:07:10 +0500

    cors

--- src/main/java/foodsave/com/foodsave/controller/UserController.java
+++ src/main/java/foodsave/com/foodsave/controller/UserController.java
@@ -22,7 +22,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@CrossOrigin(origins = "http://localhost:9527/")
+@CrossOrigin(origins =  "http://localhost:3000/")
 
 @RestController
 @RequestMapping("/api/users")
